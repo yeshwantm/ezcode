baaffbe4fcabce9a923dd6047281f5833e8607fd
Yeshwant <yeshwant.m@spintly.com>
2022-05-10 12:07:18 +0530

    first commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.send('Hello world 4?');
+    res.send('Hello world 5?');
 });
 
 app.listen(4000,()=>{
