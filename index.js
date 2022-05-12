fbfd1560d196408c7d3260428dd067a56a59785a
Yeshwant <yeshwant.m@spintly.com>
2022-05-12 16:49:28 +0530

    fix 2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.send('Hello world 12?');
+    res.send('Hello world 13?');
 });
 
 app.listen(4000,()=>{
